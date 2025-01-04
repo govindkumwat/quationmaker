bd431cebd36f21684a0ed62b4fb4ca02aba4e74f
Govind Kumawat <kumawatgovind1997@gmail.com>
2025-01-04 20:53:37 +0530

    intial commit

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
                 textDecoration: 'none',
               }}
             >
-              Jagdamba Refrigeration 
+              Jagdamba Refrigeration
             </Typography>
           </Box>
         </Toolbar>
